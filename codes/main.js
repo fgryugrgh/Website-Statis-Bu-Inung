f8b14e43fd2801d7b305a7b8710b982d97ae3a4f
fgryugrgh <154851484+fgryugrgh@users.noreply.github.com>
2026-01-08 15:05:46 +0700

    added side and navbar (not stylized)

--- codes/main.js
+++ codes/main.js
@@ -1,3 +1,22 @@
-import "jquery/dist/jquery.js"
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+let navX = -100
+let sideX = 100
+
+function dynamicNav() {
+    if (navX === 0) {
+        document.getElementById("navbarOpen").style.transform = "translateX(-100%)";
+        navX = -100
+    } else {
+        document.getElementById("navbarOpen").style.transform = "translateX(0)";
+        navX = 0
+    }
+}
+
+function dynamicSide() {
+    if (sideX === 100) {
+        document.getElementById("sidebarOpen").style.transform = "translateX(0)";
+        sideX = 0
+    } else {
+        document.getElementById("sidebarOpen").style.transform = "translateX(100%)"
+        sideX = 100
+    }
+}
