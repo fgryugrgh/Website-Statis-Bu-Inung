fbc646e36e6c3812a011dbbb5f119bb10d745bf9
fgryugrgh <154851484+fgryugrgh@users.noreply.github.com>
2026-01-15 07:53:31 +0700

    little of this little bit of that

--- codes/pages/main.js
+++ codes/pages/main.js
@@ -1,5 +1,22 @@
 //Add header and footeur
 
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  }
+});
+
 fetch('../header/header.html')
   .then(response => response.text())
   .then(data => {
@@ -14,25 +31,24 @@ fetch('../footer/footer.html')
     const button = document.getElementById('emailsubmit');
     button.addEventListener('click', () => {
     button.classList.add('running');
+    let userAddress = document.getElementById("emailform").value;
 
     setTimeout(() => {
       button.classList.remove('running');
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.onmouseenter = Swal.stopTimer;
-          toast.onmouseleave = Swal.resumeTimer;
-        }
-      });
-      Toast.fire({
-        theme: "dark",
-        icon: "success",
-        title: "Email berhasil terdaftar!"
-      });
+      if (validateEmail(userAddress) === true ) {
+        Toast.fire({
+          theme: "dark",
+          icon: "success",
+          title: "Email berhasil terdaftar!"
+        });
+      } else {
+        Toast.fire({
+          theme: "dark",
+          icon: "error",
+          title: "Email tidak valid"
+        });
+      }
     }, 500);
+    
     });
 });
