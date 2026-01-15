fe46badd637d9743d9645370d3c2393e2c846e44
fgryugrgh <154851484+fgryugrgh@users.noreply.github.com>
2026-01-15 13:50:34 +0700

    added other pages html file (no code)

--- codes/header/header.js
+++ codes/header/header.js
@@ -1,3 +1,13 @@
+const mediaQuery = window.matchMedia("(max-width: 950px)");
+
+function isMobile() {
+    if (mediaQuery.matches) {
+        return 1
+    } else {
+        return 0 
+    }
+}
+
 let navX = -100
 let sideX = 100
 
@@ -19,4 +29,23 @@ function dynamicSide() {
         document.getElementById("sidebarOpen").style.transform = "translateX(100%)"
         sideX = 100
     }
+}
+
+
+function responsiveSides(i) {
+    if (isMobile() !== 1) {
+        return i === 1 ? dynamicNav() : dynamicSide()
+    }
+
+    if (i === 1) {
+        if (sideX === 0) {
+            dynamicSide()
+        }
+        return dynamicNav()
+    }
+
+    if (navX === 0) {
+        dynamicNav()
+    }
+    dynamicSide()
 }
